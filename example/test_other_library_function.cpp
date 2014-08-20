8e9356861710493c52413164fa249b132ba5a64a
louix <gzc9047@gmail.com>
2014-08-20 21:55:05 +0800

    Resolve #7

---
+++ example/test_other_library_function.cpp
@@ -0,0 +1,39 @@
+// Copyright 2014 Louix Gu
+// Author: gzc9047@gmail.com (Louix Gu)
+
+// CppFreeMock: a tool for mock global function, member function, class static function.
+//
+// Test other library's function, make sure that we can only see the declare not definition here.
+// This test base on some standard library, so if gmock or gtest call them, this test may fail.
+// But I choose them to show that what this tool can do.
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "cpp_free_mock.h"
+
+#include <cerrno>
+#include <cstdlib>
+#include <iostream>
+
+using namespace ::std;
+using namespace ::testing;
+
+TEST(TestOtherLibraryFunction, strerror) {
+    char fakeReturn[] = "Hello world.";
+    CREATE_MOCKER(mocker, strerror);
+    EXPECT_CALL(*mocker, MOCK_FUNCTION(0))
+        .Times(Exactly(1))
+        .WillOnce(Return(fakeReturn));
+    EXPECT_EQ(fakeReturn, strerror(0));
+    mocker->RestoreToReal();
+    EXPECT_NE(fakeReturn, strerror(0));
+}
+
+TEST(TestOtherLibraryFunction, atoi) {
+    CREATE_MOCKER(mocker, atoi);
+    EXPECT_CALL(*mocker, MOCK_FUNCTION(_))
+        .Times(Exactly(1))
+        .WillOnce(Return(0));
+    EXPECT_EQ(0, atoi("1234567890"));
+}
